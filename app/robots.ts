1b02fa8f2e2e55e6b7c12128a8b2e78778e76612
Jovita <tanroel.jovita@gmail.com>
2026-07-01 11:48:21 +0800

    Add fallback sitemap for Search Console

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: `${siteUrl}/sitemap.xml`,
+    sitemap: [`${siteUrl}/sitemap.xml`, `${siteUrl}/gsc-sitemap.xml`],
   };
 }
